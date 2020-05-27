03e6a97c0e9821b7cdb98b16a50f43d8f362187d
Kir-Antipov <kp_antipov@mail.ru>
2020-05-27 03:14:52 +0300

    Added main Form

---
+++ AutoAudioSwitcher/UI/FormMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AutoAudioSwitcher.UI
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.devicesType = new System.Windows.Forms.ComboBox();
+            this.createShortcutButton = new System.Windows.Forms.Button();
+            // 
+            // devicesType
+            // 
+            this.devicesType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.devicesType.FormattingEnabled = true;
+            this.devicesType.Items.AddRange(new object[] {
+            "Output",
+            "Input"});
+            this.devicesType.Location = new System.Drawing.Point(6, 6);
+            this.devicesType.Name = "devicesType";
+            this.devicesType.Size = new System.Drawing.Size(300, 28);
+            this.devicesType.TabIndex = 1;
+            this.devicesType.SelectedIndexChanged += new System.EventHandler(this.DevicesTypeChanged);
+            // 
+            // createShortcutButton
+            // 
+            this.createShortcutButton.Enabled = false;
+            this.createShortcutButton.Location = new System.Drawing.Point(6, 248);
+            this.createShortcutButton.Name = "createShortcutButton";
+            this.createShortcutButton.Size = new System.Drawing.Size(300, 29);
+            this.createShortcutButton.TabIndex = 2;
+            this.createShortcutButton.Text = "Create enabling shortcut";
+            this.createShortcutButton.UseVisualStyleBackColor = true;
+            this.createShortcutButton.Click += new System.EventHandler(this.CreateShortcutButton_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 283);
+            this.Controls.Add(this.createShortcutButton);
+            this.Controls.Add(this.devicesType);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoAudioSwitcher";
+
+        }
+
+        #endregion
+
+        private void InitializeComponentEnd()
+        {
+            audioDevices.ItemCheck += DeviceChecked;
+            audioDevices.MouseDown += AudioDevicesMouseClicked;
+            audioDevices.ContextMenuStrip.Items[0].Click += EnableDeviceClicked;
+            Controls.Add(audioDevices);
+            devicesType.SelectedIndex = 0;
+            DoubleBuffered = true;
+        }
+
+        private System.Windows.Forms.ComboBox devicesType;
+        private Controls.MMDeviceBox audioDevices = new UI.Controls.MMDeviceBox
+        {
+            FormattingEnabled = true,
+            Location = new System.Drawing.Point(6, 40),
+            Name = "audioDevices",
+            DisplayMember = nameof(CoreAudio.MMDevice.FriendlyName),
+            ScrollAlwaysVisible = true,
+            Size = new System.Drawing.Size(300, 202),
+            TabIndex = 2,
+            SelectedDeviceBackColor = System.Drawing.Color.LightGreen,
+            ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip
+            { 
+                Items = { "Enable" } 
+            }
+        };
+        private System.Windows.Forms.SaveFileDialog fileDialog = new System.Windows.Forms.SaveFileDialog
+        {
+            AddExtension = true,
+            DefaultExt = ".lnk",
+            Filter = "*.lnk|Shortcuts",
+            ValidateNames = true,
+            Title = "Choose where to save the shortcut"
+        };
+        private System.Windows.Forms.Button createShortcutButton;
+    }
+}
---
+++ AutoAudioSwitcher/UI/FormMain.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoreAudio;
+using IWshRuntimeLibrary;
+using File = System.IO.File;
+
+namespace AutoAudioSwitcher.UI
+{
+    public sealed partial class FormMain : Form
+    {
+        #region Var
+        private static readonly EDataFlow[] DeviceTypes = { EDataFlow.eRender, EDataFlow.eRender };
+        private EDataFlow ActiveDeviceType => DeviceTypes[devicesType.SelectedIndex];
+        private IEnumerable<MMDevice> ActiveDevices => DeviceEnumerator.EnumerateAudioEndPoints(ActiveDeviceType, DEVICE_STATE.DEVICE_STATE_ACTIVE);
+
+        private readonly MMDeviceEnumerator DeviceEnumerator;
+        #endregion
+
+        #region Init
+        public FormMain()
+        {
+            InitializeComponent();
+
+            DeviceEnumerator = new MMDeviceEnumerator();
+
+            InitializeComponentEnd();
+        }
+        #endregion
+
+        #region Functions
+        private async void UpdateButtonState()
+        {
+            await Task.Delay(1);
+            createShortcutButton.Enabled = audioDevices.CheckedIndices.Count > 0;
+        }
+
+        private async void DevicesTypeChanged(object sender, EventArgs e)
+        {
+            audioDevices.Clear();
+            UpdateButtonState();
+            foreach (MMDevice device in ActiveDevices)
+                audioDevices.Items.Add(device);
+
+            await Task.Delay(1);
+            audioDevices.Focus();
+        }
+
+        private void EnableDeviceClicked(object sender, EventArgs e)
+        {
+            if (audioDevices.SelectedIndex != -1)
+            {
+                DeviceEnumerator.SetDefaultAudioEndpoint(audioDevices[audioDevices.SelectedIndex]);
+                DevicesTypeChanged(sender, e);
+            }
+        }
+
+        private void DeviceChecked(object sender, ItemCheckEventArgs e) => UpdateButtonState();
+
+        private void AudioDevicesMouseClicked(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int index = audioDevices.IndexFromPoint(e.X, e.Y);
+                if (index != ListBox.NoMatches)
+                    audioDevices.SelectedIndex = index;
+            }
+        }
+
+        private void CreateShortcutButton_Click(object sender, EventArgs e)
+        {
+            fileDialog.ShowDialog();
+            if (string.IsNullOrEmpty(fileDialog.FileName))
+                return;
+
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(fileDialog.FileName);
+            shortcut.TargetPath = Path.ChangeExtension(new Uri(typeof(Program).Assembly.CodeBase!, UriKind.Absolute).LocalPath, ".exe");
+            shortcut.WorkingDirectory = Path.GetDirectoryName(shortcut.TargetPath);
+            shortcut.Arguments = string.Join(' ', audioDevices.CheckedItems.OfType<MMDevice>().Select(x => x.ID));
+            string defaultIcons = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "SHELL32.dll");
+            if (File.Exists(defaultIcons))
+            {
+                shortcut.IconLocation = $"{defaultIcons},168";
+            }
+
+            shortcut.Save();
+        }
+        #endregion
+    }
+}
