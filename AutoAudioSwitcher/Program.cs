ecb5ac45cf98f59f647c18578beba5af8dcc6b09
Kir-Antipov <kp_antipov@mail.ru>
2020-05-27 03:02:20 +0300

    Made fast access to audio device enabling

--- AutoAudioSwitcher/Program.cs
+++ AutoAudioSwitcher/Program.cs
@@ -1,4 +1,6 @@
+using CoreAudio;
 using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace AutoAudioSwitcher
@@ -8,10 +10,31 @@ namespace AutoAudioSwitcher
         [STAThread]
         public static void Main(string[] args)
         {
+            if ((args?.Length ?? 0) == 0)
+                RunForm();
+            else
+                RunProgram(args!);
+        }
+
+        private static void RunForm()
+        {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new UI.FormMain());
+        }
+
+        private static void RunProgram(string[] args)
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            MMDevice[] devices = enumerator.EnumerateAudioEndPoints(EDataFlow.eAll, DEVICE_STATE.DEVICE_STATE_ACTIVE).ToArray();
+            devices = Array.ConvertAll(args, id => devices.FirstOrDefault(device => string.Equals(device.ID, id, StringComparison.InvariantCultureIgnoreCase)));
+
+            int selectedIndex = Array.FindIndex(devices, x => x?.Selected is true);
+            if (selectedIndex == -1)
+                selectedIndex = -1;
+
+            devices[(selectedIndex + 1) % devices.Length].Selected = true;
         }
     }
 }
